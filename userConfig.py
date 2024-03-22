4c6ce56df21e0f64c329fc2888174ddd45f11a2d
“Jacqueline <jacqueline.ilie@kcl.ac.uk>
2024-03-22 01:33:52 +0000

    changed database

---
+++ userConfig.py
@@ -0,0 +1,7 @@
+DB_CONFIG = {
+    'server': 'lexdb-dev.cwxwqmt5bnip.eu-west-2.rds.amazonaws.com',
+    'database': 'Users',
+    'username': 'hilladmin',
+    'password': 'Fg8peHWvpTZeTxuXHagi',
+    'driver': '{ODBC Driver 17 for SQL Server}'
+}
